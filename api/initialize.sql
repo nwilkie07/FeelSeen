07bcc7498637ddf5555515a7268222feb0bc25f0
nwilkie07 <nwilkie07@gmail.com>
2024-05-03 11:28:47 -0700

    switch to environment variables

--- api/initialize.sql
+++ api/initialize.sql
@@ -1,4 +1,5 @@
-CREATE TABLE IF NOT EXISTS Symptoms (id NUMERIC PRIMARY KEY, name TEXT, categoryId NUMERIC REFERENCES SymptomCategory(id));
+CREATE TABLE IF NOT EXISTS Symptoms (id NUMERIC PRIMARY KEY, name TEXT, categoryId NUMERIC REFERENCES SymptomCategory(id),
+    email TEXT REFERENCES Users(email));
 
 CREATE TABLE IF NOT EXISTS TrackedSymptoms (
       id NUMERIC,
@@ -6,7 +7,11 @@ CREATE TABLE IF NOT EXISTS TrackedSymptoms (
       symptomId NUMERIC,
       severity NUMERIC,
       note TEXT,
+      email TEXT,
       PRIMARY KEY(id, date),
-      FOREIGN KEY(symptomId) REFERENCES Symptoms(id));
+      FOREIGN KEY(symptomId) REFERENCES Symptoms(id),
+      FOREIGN KEY(email) REFERENCES Users(email));
 
-CREATE TABLE IF NOT EXISTS SymptomCategory (id NUMERIC PRIMARY KEY, color TEXT, name TEXT);
+CREATE TABLE IF NOT EXISTS SymptomCategory (id NUMERIC PRIMARY KEY, color TEXT, name TEXT, email TEXT REFERENCES Users(email));
+
+CREATE TABLE IF NOT EXISTS Users (email TEXT PRIMARY KEY, username TEXT, firstname TEXT, lastname TEXT);
